dd4ba8a2b294a25416600a8283dafe0224d8efed
Russell Cloran <rcloran@gmail.com>
2018-02-26 16:55:00 -0800

    0.0.2 -> 0.0.3

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="zigpy",
-    version="0.0.2",
+    version="0.0.3",
     description="Library implementing a ZigBee stack",
     url="http://github.com/zigpy/zigpy",
     author="Russell Cloran",
