1d4b03d8dde3cf49a345975b30bd3e4197ec42c7
Yoda-x <34125458+Yoda-x@users.noreply.github.com>
2018-04-20 14:59:54 +0200

    move new testcases to new def

--- tests/test_endpoint.py
+++ tests/test_endpoint.py
@@ -119,13 +119,16 @@ def test_cluster_attr(ep):
 def test_request(ep):
     ep.profile_id = 260
     ep.request(7, 8, b'')
+    assert ep._device.request.call_count == 1
+
+def test_request_change_profileid(ep):
     ep.profile_id = 49246
     ep.request(7, 9, b'')
     ep.profile_id = 49246
     ep.request(0x1000, 10, b'')
     ep.profile_id = 260
     ep.request(0x1000, 11, b'')
-    assert ep._device.request.call_count == 4
+    assert ep._device.request.call_count == 3
 
 
 def test_reply(ep):
