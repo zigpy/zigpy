1ff45ff2e69e7c490619148af2e56ce496b42bdf
Mark Schaumburg <mark@foresitehealthcare.com>
2018-04-23 19:44:31 -0500

    Fixed pep8 issues

--- zigpy/zcl/__init__.py
+++ zigpy/zcl/__init__.py
@@ -286,9 +286,11 @@ class Cluster(util.ListenableMixin, util.LocalLogMixin, metaclass=Registry):
         schema = foundation.COMMANDS[0x06][1]
         cfg = foundation.AttributeReportingConfig()
         cfg.direction = 0
+
         if isinstance(attribute, str):
             attribute = self._attridx[attribute]
         cfg.attrid = attribute
+
         cfg.datatype = foundation.DATA_TYPE_IDX.get(
             self.attributes.get(attribute, (None, None))[1],
             None)
