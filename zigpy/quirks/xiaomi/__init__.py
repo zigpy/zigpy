1714e671495a66c17596712f5aabe01f0b7d2897
Russell Cloran <rcloran@gmail.com>
2018-05-05 15:43:41 -0700

    Add support for Xiaomi Motion Sensor

--- zigpy/quirks/xiaomi/__init__.py
+++ zigpy/quirks/xiaomi/__init__.py
@@ -1,4 +1,7 @@
-from zigpy.quirks import CustomDevice
+import asyncio
+
+from zigpy.zcl.clusters.measurement import OccupancySensing
+from zigpy.quirks import CustomDevice, CustomCluster
 
 
 class TemperatureHumiditySensor(CustomDevice):
@@ -31,6 +34,46 @@ class TemperatureHumiditySensor(CustomDevice):
             1: {
                 'input_clusters': [0x0000, 0x0003, 0x0402, 0x0405],
             }
+        },
+    }
+
+
+class MotionSensor(CustomDevice):
+    class OccupancyCluster(CustomCluster, OccupancySensing):
+        cluster_id = 0x0406
+
+        def __init__(self, *args, **kwargs):
+            super().__init__(*args, **kwargs)
+            self._timer_handle = None
+
+        def _update_attribute(self, attrid, value):
+            super()._update_attribute(attrid, value)
+            if attrid == 0 and value == 1:
+                if self._timer_handle:
+                    self._timer_handle.cancel()
+
+                loop = asyncio.get_event_loop()
+                self._timer_handle = loop.call_later(60, self._turn_off)
+
+        def _turn_off(self):
+            self._timer_handle = None
+            self._update_attribute(self, 0, 0)
+
+    signature = {
+        # <SimpleDescriptor endpoint=1 profile=260 device_type=260 device_version=1 input_clusters=[0, 3, 65535, 25] output_clusters=[0, 4, 3, 6, 8, 5, 25]>
+        1: {
+            'profile_id': 0x0104,
+            'device_type': 0x0104,
+            'input_clusters': [0, 3, 65535, 25],
+            'output_clusters': [0, 4, 3, 6, 8, 5, 25],
+        },
+    }
+    replacement = {
+        'endpoints': {
+            1: {
+                'device_type': 0x0107,
+                'input_clusters': [0x0000, 0x0003, OccupancyCluster],
+            },
         },
     }
 
