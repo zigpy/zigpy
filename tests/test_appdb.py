646d4f0e38a6f38067132d1e0ff6e897641fb05e
David Mulcahey <david.mulcahey@icloud.com>
2019-01-21 09:51:52 -0500

    remove comment

--- tests/test_appdb.py
+++ tests/test_appdb.py
@@ -92,10 +92,6 @@ async def test_database(tmpdir):
     assert dev.endpoints[2].out_clusters[1].cluster_id == 1
     assert dev.endpoints[3].device_type == profiles.zll.DeviceType.COLOR_LIGHT
     dev = app2.get_device(custom_ieee)
-    # bad test because of bug #107 that persisted replacement signature
-    # when quirks matching reruns after a real startup this would
-    # be true
-    # assert isinstance(dev, FakeCustomDevice)
 
     app.handle_leave(99, ieee)
 
