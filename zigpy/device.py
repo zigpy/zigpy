f26ee94092c47d1e791395ce07a5c12d6e35baf5
Yoda-x <Yoda-x@github>
2019-02-15 17:41:40 +0100

    catch exception for canceling init

--- zigpy/device.py
+++ zigpy/device.py
@@ -45,46 +45,55 @@ class Device(zigpy.util.LocalLogMixin):
 
     def schedule_initialize(self):
         if self.initializing:
-            LOGGER.debug("Canceling old initialize call")
+            LOGGER.debug("[0x%04x] Canceling old initialize call",  self.nwk)
             self._init_handle.cancel()
         else:
             self.initializing = True
         self._init_handle = asyncio.ensure_future(self._initialize())
 
     async def _initialize(self):
-        if self.status == Status.NEW:
-            self.info("[0x%04x] Discovering endpoints", self.nwk)
-            try:
-                epr = await self.zdo.request(0x0005, self.nwk, tries=3)
-                if epr[0] != 0:
-                    raise Exception("[0x%04x] Endpoint request failed: %s", self.nwk, epr)
-            except Exception as exc:
-                self.initializing = False
-                LOGGER.exception("[0x%04x] Failed ZDO request during device initialization: %s", self.nwk, exc)
-                return
+        try:
+            if self.status == Status.NEW:
+                self.info("[0x%04x] Discovering endpoints", self.nwk)
+                try:
+                    epr = await self.zdo.request(0x0005, self.nwk, tries=3)
+                    if epr[0] != 0:
+                        raise Exception(
+                            "[0x%04x] Endpoint request failed: %s", self.nwk, epr
+                        )
+                except Exception as exc:
+                    self.initializing = False
+                    LOGGER.exception(
+                        "[0x%04x] Failed ZDO request during device initialization: %s",
+                        self.nwk, exc
+                    )
+                    return
 
-            self.info("[0x%04x] Discovered endpoints: %s", self.nwk, epr[2])
+                self.info("[0x%04x] Discovered endpoints: %s", self.nwk, epr[2])
 
-            for endpoint_id in epr[2]:
-                self.add_endpoint(endpoint_id)
+                for endpoint_id in epr[2]:
+                    self.add_endpoint(endpoint_id)
 
-            self.status = Status.ZDO_INIT
+                self.status = Status.ZDO_INIT
 
-        for endpoint_id in self.endpoints.keys():
-            if endpoint_id == 0:  # ZDO
-                continue
-            await self.endpoints[endpoint_id].initialize()
+            for endpoint_id in self.endpoints.keys():
+                if endpoint_id == 0:  # ZDO
+                    continue
+                await self.endpoints[endpoint_id].initialize()
 
-        self.status = Status.ENDPOINTS_INIT
-        self.initializing = False
-        self._application.device_initialized(self)
+            self.status = Status.ENDPOINTS_INIT
+            self.initializing = False
+            self._application.device_initialized(self)
+        except asyncio.CancelledError:
+            LOGGER.debug("[0x%04x] Catched CanceledError",  self.nwk)
 
     def add_endpoint(self, endpoint_id):
         ep = zigpy.endpoint.Endpoint(self, endpoint_id)
         self.endpoints[endpoint_id] = ep
         return ep
 
-    async def request(self, profile, cluster, src_ep, dst_ep, sequence, data, expect_reply=True,  timeout = 15):
+    async def request(self, profile, cluster, src_ep, dst_ep, sequence, data,
+                      expect_reply=True,  timeout=15):
         if self.type & 8:
             timeout = 5
         result = await self._application.request(
@@ -107,7 +116,8 @@ class Device(zigpy.util.LocalLogMixin):
     def deserialize(self, endpoint_id, cluster_id, data):
         return self.endpoints[endpoint_id].deserialize(cluster_id, data)
 
-    def handle_message(self, is_reply, profile, cluster, src_ep, dst_ep, tsn, command_id, args):
+    def handle_message(self, is_reply, profile, cluster, src_ep, dst_ep, tsn,
+                           command_id, args):
         self.last_seen = time.time()
         try:
             endpoint = self.endpoints[src_ep]
@@ -118,13 +128,15 @@ class Device(zigpy.util.LocalLogMixin):
             )
             return
 
-        return endpoint.handle_message(is_reply, profile, cluster, tsn, command_id, args)
+        return endpoint.handle_message(is_reply, profile, cluster, tsn,
+                                       command_id, args)
 
     def handle_RouteRecord(self, path):
         self.path = path
 
     def reply(self, profile, cluster, src_ep, dst_ep, sequence, data):
-        return self._application.request(self.nwk, profile, cluster, src_ep, dst_ep, sequence, data, False)
+        return self._application.request(self.nwk, profile, cluster, src_ep,
+                                         dst_ep, sequence, data, False)
 
     def radio_details(self, lqi, rssi):
         self.lqi = lqi
