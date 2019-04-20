20467d27865b5a66d33e0123059e33d04ed547fd
damarco <schmidt.d@aon.at>
2019-04-20 20:54:31 +0200

    0.3.1 -> 0.3.2

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="zigpy-homeassistant",
-    version="0.3.1",
+    version="0.3.2",
     description="Library implementing a ZigBee stack",
     url="http://github.com/zigpy/zigpy",
     author="Russell Cloran",
