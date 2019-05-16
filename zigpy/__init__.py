30030fbe96442b07b2cd783dfa4b8dd7d3d3e9d1
Alexei Chetroi <lexoid@gmail.com>
2019-05-16 17:44:22 -0400

    0.3.4.dev0 -> 0.5.0.dev0

--- zigpy/__init__.py
+++ zigpy/__init__.py
@@ -1,6 +1,6 @@
 # coding: utf-8
 MAJOR_VERSION = 0
-MINOR_VERSION = 3
-PATCH_VERSION = '4.dev0'
+MINOR_VERSION = 5
+PATCH_VERSION = '0.dev0'
 __short_version__ = '{}.{}'.format(MAJOR_VERSION, MINOR_VERSION)
 __version__ = '{}.{}'.format(__short_version__, PATCH_VERSION)
