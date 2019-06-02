9af66adb6a3053cf82d2197d1032283d4e1b16ff
Alexei Chetroi <alexei.chetroi@outlook.com>
2019-06-02 01:30:37 -0400

    OTA image header class.

--- tests/test_ota_firmware.py
+++ tests/test_ota_firmware.py
@@ -2,7 +2,8 @@ from unittest import mock
 
 import pytest
 
-from zigpy.ota.firmware import Firmware, FirmwareKey
+import zigpy.ota.firmware as firmware
+import zigpy.types as t
 
 MANUFACTURER_ID = mock.sentinel.manufacturer_id
 IMAGE_TYPE = mock.sentinel.image_type
@@ -10,18 +11,18 @@ IMAGE_TYPE = mock.sentinel.image_type
 
 @pytest.fixture
 def key():
-    return FirmwareKey(MANUFACTURER_ID, IMAGE_TYPE)
+    return firmware.FirmwareKey(MANUFACTURER_ID, IMAGE_TYPE)
 
 
 def test_firmware_key():
-    key = FirmwareKey(MANUFACTURER_ID, IMAGE_TYPE)
+    key = firmware.FirmwareKey(MANUFACTURER_ID, IMAGE_TYPE)
     assert key.manufacturer_id is MANUFACTURER_ID
     assert key.image_type is IMAGE_TYPE
 
 
 def test_firmware(key):
     s = mock.sentinel
-    frm = Firmware(key, s.ver, s.size, s.url)
+    frm = firmware.Firmware(key, s.ver, s.size, s.url)
 
     assert frm.key == key
     assert frm.version is s.ver
@@ -35,8 +36,129 @@ def test_firmware(key):
 
 def test_upgradeable(key):
     s = mock.sentinel
-    frm = Firmware(key, 100, s.size, s.url, s.data)
+    frm = firmware.Firmware(key, 100, s.size, s.url, s.data)
 
     assert not frm.upgradeable(MANUFACTURER_ID, IMAGE_TYPE, 100, None)
     assert not frm.upgradeable(MANUFACTURER_ID, IMAGE_TYPE, 101, None)
     assert frm.upgradeable(MANUFACTURER_ID, IMAGE_TYPE, 99, None)
+
+
+def test_hw_version():
+    hw = firmware.HWVersion(0x0a01)
+    assert hw.version == 10
+    assert hw.revision == 1
+
+    assert 'version=10' in repr(hw)
+    assert 'revision=1' in repr(hw)
+
+
+def _test_ota_img_header(field_control, hdr_suffix=b'', extra=b''):
+    d = b'\x1e\xf1\xee\x0b\x00\x018\x00'
+    d += field_control
+    d += b'|\x11\x01!rE!\x12\x02\x00EBL tradfri_light_basic\x00\x00\x00' \
+         b'\x00\x00\x00\x00\x00\x00~\x91\x02\x00'
+    d += hdr_suffix
+
+    hdr, rest = firmware.OTAImageHeader.deserialize(d + extra)
+    assert hdr.header_version == 0x0100
+    assert hdr.header_length == 0x0038
+    assert hdr.manufacturer_id == 4476
+    assert hdr.image_type == 0x2101
+    assert hdr.file_version == 0x12214572
+    assert hdr.stack_version == 0x0002
+    assert hdr.image_size == 0x0002917e
+    assert hdr.serialize() == d
+
+    return hdr, rest
+
+
+def test_ota_image_header():
+    hdr = firmware.OTAImageHeader()
+    assert hdr.security_credential_version_present is None
+    assert hdr.device_specific_file is None
+    assert hdr.hardware_versions_present is None
+
+    extra = b'abcdefghklmnpqr'
+
+    hdr, rest = _test_ota_img_header(b'\x00\x00', extra=extra)
+    assert rest == extra
+    assert hdr.security_credential_version_present is False
+    assert hdr.device_specific_file is False
+    assert hdr.hardware_versions_present is False
+
+
+def test_ota_image_header_security():
+    extra = b'abcdefghklmnpqr'
+    creds = t.uint8_t(0xac)
+    hdr, rest = _test_ota_img_header(b'\x01\x00', creds.serialize(), extra)
+
+    assert rest == extra
+    assert hdr.security_credential_version_present is True
+    assert hdr.security_credential_version == creds
+    assert hdr.device_specific_file is False
+    assert hdr.hardware_versions_present is False
+
+
+def test_ota_image_header_hardware_versions():
+    extra = b'abcdefghklmnpqr'
+    hw_min = firmware.HWVersion(0xbeef)
+    hw_max = firmware.HWVersion(0xabcd)
+    hdr, rest = _test_ota_img_header(b'\x04\x00',
+                                     hw_min.serialize() + hw_max.serialize(),
+                                     extra)
+
+    assert rest == extra
+    assert hdr.security_credential_version_present is False
+    assert hdr.device_specific_file is False
+    assert hdr.hardware_versions_present is True
+    assert hdr.minimum_hardware_version == hw_min
+    assert hdr.maximum_hardware_version == hw_max
+
+
+def test_ota_image_destination():
+    extra = b'abcdefghklmnpqr'
+
+    dst = t.EUI64.deserialize(b'12345678')[0]
+
+    hdr, rest = _test_ota_img_header(b'\x02\x00', dst.serialize(), extra)
+    assert rest == extra
+    assert hdr.security_credential_version_present is False
+    assert hdr.device_specific_file is True
+    assert hdr.upgrade_file_destination == dst
+    assert hdr.hardware_versions_present is False
+
+
+def test_ota_img_wrong_header():
+    d = b'\x1e\xf0\xee\x0b\x00\x018\x00\x00\x00'
+    d += b'|\x11\x01!rE!\x12\x02\x00EBL tradfri_light_basic\x00\x00\x00' \
+         b'\x00\x00\x00\x00\x00\x00~\x91\x02\x00'
+
+    with pytest.raises(ValueError):
+        firmware.OTAImageHeader.deserialize(d)
+
+    with pytest.raises(ValueError):
+        firmware.OTAImageHeader.deserialize(d + b'123abc')
+
+
+def test_header_string():
+    size = 32
+    header_string = "This is a header String"
+    data = header_string.encode('utf8').ljust(size, b'\x00')
+    extra = b'cdef123'
+
+    hdr_str, rest = firmware.HeaderString.deserialize(data + extra)
+    assert rest == extra
+    assert hdr_str == header_string
+
+    hdr_str, rest = firmware.HeaderString.deserialize(data)
+    assert rest == b''
+    assert hdr_str == header_string
+    assert firmware.HeaderString(header_string).serialize() == data
+
+
+def test_header_string_too_short():
+    header_string = "This is a header String"
+    data = header_string.encode('utf8')
+
+    with pytest.raises(ValueError):
+        firmware.HeaderString.deserialize(data)
--- zigpy/ota/firmware.py
+++ zigpy/ota/firmware.py
@@ -29,3 +29,105 @@ class Firmware:
         """Return True if firmware validation passes."""
         # ToDo proper validation
         return self.data is not None
+
+
+class HWVersion(t.uint16_t):
+    @property
+    def version(self):
+        return self >> 8
+
+    @property
+    def revision(self):
+        return self & 0x00ff
+
+    def __repr__(self):
+        return "<{} version={} revision={}>".format(self.__class__.__name__,
+                                                    self.version,
+                                                    self.revision)
+
+
+class HeaderString(str):
+    _size = 32
+
+    @classmethod
+    def deserialize(cls, data):
+        if len(data) < cls._size:
+            raise ValueError("Data is too short. Should be at least %s",
+                             cls._size)
+        raw = data[:cls._size].split(b'\x00')[0]
+        return cls(raw.decode('utf8', errors='replace')), data[cls._size:]
+
+    def serialize(self):
+        return self.encode('utf8').ljust(self._size, b'\x00')
+
+
+class OTAImageHeader(t.Struct):
+    MAGIC_VALUE = 0x0beef11e
+    _fields = [
+        ('upgrade_file_id', t.uint32_t),
+        ('header_version', t.uint16_t),
+        ('header_length', t.uint16_t),
+        ('field_control', t.uint16_t),
+        ('manufacturer_id', t.uint16_t),
+        ('image_type', t.uint16_t),
+        ('file_version', t.uint32_t),
+        ('stack_version', t.uint16_t),
+        ('header_string', HeaderString),
+        ('image_size', t.uint32_t)
+    ]
+
+    @property
+    def security_credential_version_present(self) -> bool:
+        if self.field_control is None:
+            return None
+        return bool(self.field_control & 0x01)
+
+    @property
+    def device_specific_file(self) -> bool:
+        if self.field_control is None:
+            return None
+        return bool(self.field_control & 0x02)
+
+    @property
+    def hardware_versions_present(self) -> bool:
+        if self.field_control is None:
+            return None
+        return bool(self.field_control & 0x04)
+
+    @classmethod
+    def deserialize(cls, data) -> tuple:
+        hdr, data = super().deserialize(data)
+        if hdr.upgrade_file_id != cls.MAGIC_VALUE:
+            raise ValueError("Wrong magic number for OTA Image: %s" %
+                             (hdr.upgrade_file_id, ))
+
+        if hdr.security_credential_version_present:
+            hdr.security_credential_version, data = t.uint8_t.deserialize(data)
+        else:
+            hdr.security_credential_version = None
+
+        if hdr.device_specific_file:
+            hdr.upgrade_file_destination, data = t.EUI64.deserialize(data)
+        else:
+            hdr.upgrade_file_destination = None
+
+        if hdr.hardware_versions_present:
+            hdr.minimum_hardware_version, data = HWVersion.deserialize(data)
+            hdr.maximum_hardware_version, data = HWVersion.deserialize(data)
+        else:
+            hdr.minimum_hardware_version = None
+            hdr.maximum_hardware_version = None
+
+        return hdr, data
+
+    def serialize(self) -> bytes:
+        data = super().serialize()
+        if self.security_credential_version_present:
+            data += self.security_credential_version.serialize()
+        if self.device_specific_file:
+            data += self.upgrade_file_destination.serialize()
+        if self.hardware_versions_present:
+            data += self.minimum_hardware_version.serialize()
+            data += self.maximum_hardware_version.serialize()
+        return data
+
