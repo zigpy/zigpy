df24df1e930b172952e2844122ed8a40c52f69c5
Alexei Chetroi <lexoid@gmail.com>
2019-06-22 22:34:56 -0400

    0.6.1 Release. (#189)
    * 0.6.1 version bump
    * Don't override endpoint's manufacturer/model from quirks. (#187)

--- zigpy/quirks/__init__.py
+++ zigpy/quirks/__init__.py
@@ -76,8 +76,6 @@ class CustomEndpoint(zigpy.endpoint.Endpoint):
 
         set_device_attr('profile_id')
         set_device_attr('device_type')
-        set_device_attr('manufacturer')
-        set_device_attr('model')
         self.status = zigpy.endpoint.Status.ZDO_INIT
 
         for c in replacement_data.get('input_clusters', []):
