cdba886509355c9b0b184f16847422adedb6d4ab
Yoda-x <Yoda-x@github>
2019-08-04 23:31:26 +0200

    bump version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="zigpy",
-    version="100.1.4.9",
+    version="100.1.4.10",
     description="Library implementing a ZigBee stack",
     url="http://github.com/zigpy/zigpy",
     author="orig byRussell Cloran",
