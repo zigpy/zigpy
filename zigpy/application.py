e5ed489f953f0961c36b348cf7c4294f0a58334b
David Mulcahey <david.mulcahey@me.com>
2019-08-09 21:01:15 -0400

    don't rediscover on nwk swap

--- zigpy/application.py
+++ zigpy/application.py
@@ -91,6 +91,8 @@ class ControllerApplication(zigpy.util.ListenableMixin):
             if dev.nwk != nwk:
                 LOGGER.debug("Device %s changed id (0x%04x => 0x%04x)", ieee, dev.nwk, nwk)
                 dev.nwk = nwk
+                self.device_initialized(dev)
+                return
             elif dev.initializing or dev.status == zigpy.device.Status.ENDPOINTS_INIT:
                 LOGGER.debug("Skip initialization for existing device %s", ieee)
                 return
