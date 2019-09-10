7c2293d180e7860f57d91029d74d2a8232534b2f
Alexei Chetroi <lexoid@gmail.com>
2019-09-10 10:13:53 -0400

    0.8.0 Version bump.

--- zigpy/__init__.py
+++ zigpy/__init__.py
@@ -1,6 +1,6 @@
 # coding: utf-8
 MAJOR_VERSION = 0
 MINOR_VERSION = 8
-PATCH_VERSION = "0.dev0"
+PATCH_VERSION = "0"
 __short_version__ = "{}.{}".format(MAJOR_VERSION, MINOR_VERSION)
 __version__ = "{}.{}".format(__short_version__, PATCH_VERSION)
