c7777c467777d03ba6bb1b5e59896abb892bbe05
James Riley <jriley@jamespriley.com>
2019-09-14 17:47:51 -0600

    Add new Samsung Multisensor
    - This unit has an additional input ID, and thus doesn't match
      the existing setup.
    - Confirmed working with zha in HomeAssistant on Nortek HUSBZB-1

--- zigpy/quirks/smartthings/__init__.py
+++ zigpy/quirks/smartthings/__init__.py
@@ -100,3 +100,46 @@ class SmartthingsMultiPurposeSensor(CustomDevice):
             }
         }
     }
+
+
+class SmartthingsMultiPurposeSensor2019(CustomDevice):
+    signature = {
+        "endpoints": {
+            # <SimpleDescriptor endpoint=1 profile=260 device_type=1026
+            # device_version=0 input_clusters=[0, 1, 3, 32, 1026, 1280, 2821, 64514]
+            # output_clusters=[3, 25]>
+            1: {
+                "profile_id": 0x0104,
+                "device_type": 0x0402,
+                "input_clusters": [
+                    0,
+                    1,
+                    3,
+                    32,
+                    1026,
+                    1280,
+                    2821,
+                    SmartThingsAccelCluster.cluster_id,
+                ],
+                "output_clusters": [3, 25],
+            }
+        }
+    }
+
+    replacement = {
+        "endpoints": {
+            1: {
+                "input_clusters": [
+                    0x0000,
+                    0x0001,
+                    0x0003,
+                    0x0020,
+                    0x0402,
+                    0x0500,
+                    0x0B05,
+                    SmartThingsAccelCluster,
+                ],
+                "output_clusters": [0x0003, 0x0019],
+            }
+        }
+    }
