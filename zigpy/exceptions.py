3b6d6a4944fa4ec1f9cd62d0ae975d5d6d84289f
Alexei Chetroi <lexoid@gmail.com>
2020-03-25 17:20:15 -0400

    Add new Exceptions (#334)
    Add Exception for ControllerApplication and API layers.

--- zigpy/exceptions.py
+++ zigpy/exceptions.py
@@ -2,5 +2,17 @@ class ZigbeeException(Exception):
     """Base exception class"""
 
 
+class ControllerException(ZigbeeException):
+    """Application controller failed in some way."""
+
+    pass
+
+
+class APIException(ZigbeeException):
+    """Radio API failed in some way."""
+
+    pass
+
+
 class DeliveryError(ZigbeeException):
     """Message delivery failed in some way"""
