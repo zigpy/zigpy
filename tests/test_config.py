34f4ea7bb5109a770a772b0da490adb4a8a3c5ce
Alexei Chetroi <alexei.chetroi@outlook.com>
2020-04-04 23:16:46 -0400

    Bool validation method.

---
+++ tests/test_config.py
@@ -0,0 +1,38 @@
+"""Test configuration."""
+
+import pytest
+import voluptuous as vol
+import zigpy.config
+
+
+@pytest.mark.parametrize(
+    "value, result",
+    [
+        (False, False),
+        (True, True),
+        ("1", True),
+        ("yes", True),
+        ("YeS", True),
+        ("on", True),
+        ("oN", True),
+        ("enable", True),
+        ("enablE", True),
+        (0, False),
+        ("no", False),
+        ("nO", False),
+        ("off", False),
+        ("ofF", False),
+        ("disable", False),
+        ("disablE", False),
+    ],
+)
+def test_config_validation_bool(value, result):
+    """Test boolean config validation."""
+    assert zigpy.config.cv_boolean(value) is result
+
+
+@pytest.mark.parametrize("value", ["invalid", "not a bool", "something"])
+def test_config_validation_bool_invalid(value):
+    """Test boolean config validation."""
+    with pytest.raises(vol.Invalid):
+        zigpy.config.cv_boolean(value)
--- zigpy/config.py
+++ zigpy/config.py
@@ -1,4 +1,6 @@
 """Config schemas and validation."""
+from typing import Union
+
 import voluptuous as vol
 
 CONF_DATABASE = "database_path"
@@ -23,3 +25,18 @@ SCHEMA = {
 }
 
 CONFIG_SCHEMA = vol.Schema(SCHEMA, extra=vol.ALLOW_EXTRA)
+
+
+def cv_boolean(value: Union[bool, int, str]) -> bool:
+    """Validate and coerce a boolean value."""
+    if isinstance(value, bool):
+        return value
+    if isinstance(value, str):
+        value = value.lower().strip()
+        if value in ("1", "true", "yes", "on", "enable"):
+            return True
+        if value in ("0", "false", "no", "off", "disable"):
+            return False
+    elif isinstance(value, int):
+        return bool(value)
+    raise vol.Invalid("invalid boolean value {}".format(value))
