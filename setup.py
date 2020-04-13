2c5f21fab2b5c3f11427957c136856fec7c7156d
Alexei Chetroi <lexoid@gmail.com>
2020-04-12 20:14:03 -0400

    Add long description to PyPi index (#371)

--- setup.py
+++ setup.py
@@ -1,7 +1,13 @@
 """Setup module for zigpy"""
 
+from os import path
+
 from setuptools import find_packages, setup
 import zigpy
+
+this_directory = path.join(path.abspath(path.dirname(__file__)))
+with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
 
 setup(
     name="zigpy",
