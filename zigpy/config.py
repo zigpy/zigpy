9f1fd7cd1f3478864061a2036b83ccdce00796d6
Alexei Chetroi <alexei.chetroi@outlook.com>
2020-04-19 00:45:30 -0400

    Add NETWORK and NETWORK_UPDATE schemas.

--- zigpy/config.py
+++ zigpy/config.py
@@ -2,10 +2,66 @@
 from typing import Union
 
 import voluptuous as vol
+import zigpy.types as t
 
 CONF_DATABASE = "database_path"
 CONF_DEVICE = "device"
 CONF_DEVICE_PATH = "path"
+CONF_NWK = "network"
+CONF_NWK_CHANNEL = "channel"
+CONF_NWK_CHANNEL_DEFAULT = 15
+CONF_NWK_CHANNELS = "channels"
+CONF_NWK_CHANNELS_DEFAULT = [15, 20, 25]
+CONF_NWK_EXTENDED_PAN_ID = "extended_pan_id"
+CONF_NWK_EXTENDED_PAN_ID_DEFAULT = t.ExtendedPanId(
+    [0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]
+)
+CONF_NWK_PAN_ID = "pan_id"
+CONF_NWK_PAN_ID_DEFAULT = t.PanId(0x0000)
+CONF_NWK_KEY = "key"
+CONF_NWK_KEY_DEFAULT = t.KeyData(
+    [
+        0x00,
+        0x00,
+        0x00,
+        0x00,
+        0x00,
+        0x00,
+        0x00,
+        0x00,
+        0x00,
+        0x00,
+        0x00,
+        0x00,
+        0x00,
+        0x00,
+        0x00,
+        0x00,
+    ]
+)
+CONF_NWK_TC_LINK_KEY = "tc_link_key"
+CONF_NWK_TC_LINK_KEY_DEFAULT = t.KeyData(
+    [
+        0x5A,
+        0x69,
+        0x67,
+        0x42,
+        0x65,
+        0x65,
+        0x41,
+        0x6C,
+        0x6C,
+        0x69,
+        0x61,
+        0x6E,
+        0x63,
+        0x65,
+        0x30,
+        0x39,
+    ]
+)
+CONF_NWK_UPDATE_ID = "update_id"
+CONF_NWK_UPDATE_ID_DEFAULT = 0x00
 CONF_OTA = "ota"
 CONF_OTA_DIR = "otau_directory"
 CONF_OTA_IKEA = "ikea_provider"
@@ -28,6 +84,39 @@ def cv_boolean(value: Union[bool, int, str]) -> bool:
 
 
 SCHEMA_DEVICE = vol.Schema({vol.Required(CONF_DEVICE_PATH): str})
+SCHEMA_NETWORK = vol.Schema(
+    {
+        vol.Optional(CONF_NWK_CHANNEL, default=CONF_NWK_CHANNEL_DEFAULT): vol.All(
+            int, vol.Range(min=11, max=26)
+        ),
+        vol.Optional(CONF_NWK_CHANNELS, default=CONF_NWK_CHANNELS_DEFAULT): vol.All(
+            list, t.Channels.from_channel_list
+        ),
+        vol.Optional(CONF_NWK_PAN_ID, default=CONF_NWK_PAN_ID_DEFAULT): vol.Coerce(
+            t.PanId
+        ),
+        vol.Optional(
+            CONF_NWK_EXTENDED_PAN_ID, default=CONF_NWK_EXTENDED_PAN_ID_DEFAULT
+        ): t.ExtendedPanId,
+        vol.Optional(CONF_NWK_UPDATE_ID, default=CONF_NWK_UPDATE_ID_DEFAULT): vol.Range(
+            min=0, max=255
+        ),
+        vol.Optional(CONF_NWK_KEY, default=CONF_NWK_KEY_DEFAULT): vol.All(
+            list, vol.Coerce(t.KeyData)
+        ),
+    }
+)
+SCHEMA_NETWORK_UPDATE = vol.Schema(
+    {
+        vol.Optional(CONF_NWK_CHANNEL): vol.All(int, vol.Range(min=11, max=26)),
+        vol.Optional(CONF_NWK_CHANNELS): vol.All(list, t.Channels.from_channel_list),
+        vol.Optional(CONF_NWK_PAN_ID): vol.Coerce(t.PanId),
+        vol.Optional(CONF_NWK_EXTENDED_PAN_ID): t.ExtendedPanId,
+        vol.Optional(CONF_NWK_UPDATE_ID): vol.Range(min=0, max=255),
+        vol.Optional(CONF_NWK_KEY): vol.All(list, vol.Coerce(t.KeyData)),
+    }
+)
+
 SCHEMA_OTA = {
     vol.Optional(CONF_OTA_IKEA, default=False): cv_boolean,
     vol.Optional(CONF_OTA_LEDVANCE, default=False): cv_boolean,
@@ -37,6 +126,7 @@ SCHEMA_OTA = {
 ZIGPY_SCHEMA = vol.Schema(
     {
         vol.Optional(CONF_DATABASE, default=None): vol.Any(None, str),
+        vol.Optional(CONF_NWK, default={}): SCHEMA_NETWORK,
         vol.Optional(CONF_OTA, default={}): SCHEMA_OTA,
     },
     extra=vol.ALLOW_EXTRA,
