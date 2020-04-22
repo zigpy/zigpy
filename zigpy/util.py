ce989c477bfcfb48c0bc379cc4e28d59f95855e2
Alexei Chetroi <alexei.chetroi@outlook.com>
2020-04-22 12:46:56 -0400

    Use logger for retries logging.

--- zigpy/util.py
+++ zigpy/util.py
@@ -102,7 +102,7 @@ async def retry(func, retry_exceptions, tries=3, delay=0.1):
     Only exceptions in `retry_exceptions` will be retried.
     """
     while True:
-        print("Tries remaining: %s" % (tries,))
+        LOGGER.debug("Tries remaining: %s", tries)
         try:
             r = await func()
             return r
