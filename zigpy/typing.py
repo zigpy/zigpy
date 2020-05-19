4f46cd09ad46755c806234c6f29c66969bc334ee
Alexei Chetroi <alexei.chetroi@outlook.com>
2020-05-19 18:21:46 -0400

    Drop support for legacy quirks signature.

--- zigpy/typing.py
+++ zigpy/typing.py
@@ -5,12 +5,16 @@ from typing import TYPE_CHECKING
 import zigpy.application
 import zigpy.device
 import zigpy.endpoint
+import zigpy.quirks
 import zigpy.zcl
 import zigpy.zdo
 
 # pylint: disable=invalid-name
 ClusterType = "Cluster"
 ControllerApplicationType = "ControllerApplication"
+CustomClusterType = "CustomCluster"
+CustomDeviceType = "CustomDevice"
+CustomEndpointType = "CustomEndpoint"
 DeviceType = "Device"
 EndpointType = "Endpoint"
 ZDOType = "ZDO"
@@ -18,6 +22,9 @@ ZDOType = "ZDO"
 if TYPE_CHECKING:
     ClusterType = zigpy.zcl.Cluster
     ControllerApplicationType = zigpy.application.ControllerApplication
+    CustomClusterType = zigpy.quirks.CustomCluster
+    CustomDeviceType = zigpy.quirks.CustomDevice
+    CustomEndpointType = zigpy.quirks.CustomEndpoint
     DeviceType = zigpy.device.Device
     EndpointType = zigpy.endpoint.Endpoint
     ZDOType = zigpy.zdo.ZDO
