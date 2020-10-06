238ca8520424f0090ef2f325179461012c38e90e
Alexei Chetroi <lexoid@gmail.com>
2020-10-06 18:09:03 -0400

    0.27.0.dev0 version bump

--- zigpy/__init__.py
+++ zigpy/__init__.py
@@ -1,6 +1,6 @@
 # coding: utf-8
 MAJOR_VERSION = 0
-MINOR_VERSION = 26
+MINOR_VERSION = 27
 PATCH_VERSION = "0.dev0"
 __short_version__ = "{}.{}".format(MAJOR_VERSION, MINOR_VERSION)
 __version__ = "{}.{}".format(__short_version__, PATCH_VERSION)
