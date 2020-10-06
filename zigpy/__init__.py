d20c0e57f05433c6b3c7a897d83f798dc5d31aaf
Alexei Chetroi <lexoid@gmail.com>
2020-10-06 18:10:04 -0400

    Use f-strings for versions

--- zigpy/__init__.py
+++ zigpy/__init__.py
@@ -2,5 +2,5 @@
 MAJOR_VERSION = 0
 MINOR_VERSION = 27
 PATCH_VERSION = "0.dev0"
-__short_version__ = "{}.{}".format(MAJOR_VERSION, MINOR_VERSION)
-__version__ = "{}.{}".format(__short_version__, PATCH_VERSION)
+__short_version__ = f"{MAJOR_VERSION}.{MINOR_VERSION}"
+__version__ = f"{__short_version__}.{PATCH_VERSION}"
