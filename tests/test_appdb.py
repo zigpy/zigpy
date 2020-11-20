f42f5d83270819bcd1782f93d54979260c824f4d
Alexei Chetroi <alexei.chetroi@outlook.com>
2020-11-20 17:28:53 -0500

    Fix appdb tests

--- tests/test_appdb.py
+++ tests/test_appdb.py
@@ -65,7 +65,6 @@ def mock_dev_init(status: Status):
 
     def _initialize(self):
         self.status = status
-        self.initializing = False
         self.node_desc = zdo_t.NodeDescriptor(0, 1, 2, 3, 4, 5, 6, 7, 8)
 
     return _initialize
