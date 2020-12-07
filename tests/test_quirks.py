zigpy/zigpy

commit 00f22892cec78ec3528a5d8276f2c1d97811f036

Framework implementing message handlers from uninitialized devices (#555)
* Keep quirks list per model in a separate dict

* Fix typing

* Coverage

* Framework to handler messages from uninitilized devices

* Fire 'handle_message' event on incoming traffic

* Allow device initialization to be cancelled externally

* Fix appdb tests

* coverage

zigpy/quirks/__init__.py
@@ -33,11 +33,29 @@ import zigpy.zcl.foundation as foundation
 _LOGGER = logging.getLogger(__name__)
 
 _DEVICE_REGISTRY = DeviceRegistry()
+_uninitialized_device_message_handlers = []
 
 
 def get_device(device, registry=_DEVICE_REGISTRY):
     """Get a CustomDevice object, if one is available"""
     return registry.get_device(device)
+
+
+def get_model_quirks(
+    model: str, registry: DeviceRegistry = _DEVICE_REGISTRY
+) -> List["CustomDevice"]:
+    """Get all quirks for given model."""
+    return registry.model_quirks.get(model)
+
+
+def register_uninitialized_device_message_handler(handler: Callable) -> None:
+    """Register an handler for messages received by uninitialized devices.
+
+    each handler is passed same parameters as
+    zigpy.application.ControllerApplication.handle_message
+    """
+    if handler not in _uninitialized_device_message_handlers:
+        _uninitialized_device_message_handlers.append(handler)
 
 
 class Registry(type):
@@ -249,3 +267,17 @@ class CustomCluster(zigpy.zcl.Cluster):
         if manufacturer is None and self._has_manuf_attr([a.attrid for a in args]):
             manufacturer = self.endpoint.manufacturer_id
         return super()._write_attributes_undivided(args, manufacturer=manufacturer)
+
+
+def handle_message_from_uninitialized_sender(
+    sender: zigpy.device.Device,
+    profile: int,
+    cluster: int,
+    src_ep: int,
+    dst_ep: int,
+    message: bytes,
+) -> None:
+    """Processes message from an uninitialized sender."""
+    for handler in _uninitialized_device_message_handlers:
+        if handler(sender, profile, cluster, src_ep, dst_ep, message):
+            break

zigpy/quirks/registry.py
@@ -1,7 +1,7 @@
 import collections
 import itertools
 import logging
-from typing import Union
+from typing import Dict, List, Union
 
 import zigpy.quirks
 from zigpy.typing import CustomDeviceType, DeviceType
@@ -19,11 +19,16 @@ SIG_MODELS_INFO = "models_info"
 SIG_NODE_DESC = "node_desc"
 SIG_SKIP_CONFIG = "skip_configuration"
 
+TYPE_MODEL_QUIRKS_LIST = Dict[str, List["zigpy.quirks.CustomDevice"]]
+TYPE_MANUF_QUIRKS_DICT = Dict[str, TYPE_MODEL_QUIRKS_LIST]
+
 
 class DeviceRegistry:
     def __init__(self, *args, **kwargs):
-        dd = collections.defaultdict
-        self._registry = dd(lambda: dd(list))
+        self._model_quirks: TYPE_MODEL_QUIRKS_LIST = collections.defaultdict(list)
+        self._registry: TYPE_MANUF_QUIRKS_DICT = collections.defaultdict(
+            lambda: self._model_quirks
+        )
 
     def add_to_registry(self, custom_device: CustomDeviceType) -> None:
         """Add a device to the registry"""
@@ -151,6 +156,11 @@ class DeviceRegistry:
     @property
     def registry(self):
         return self._registry
+
+    @property
+    def model_quirks(self) -> TYPE_MODEL_QUIRKS_LIST:
+        """Return a list of quirks for a given model."""
+        return self._model_quirks
 
     def __contains__(self, device: CustomDeviceType) -> bool:
         manufacturer, model = device.signature.get(

tests/test_quirks.py
@@ -657,3 +657,28 @@ async def test_configure_reporting_manufacture_specific(
         )
         assert cmd_mock.call_count == 1
         assert cmd_mock.call_args[1]["manufacturer"] is sentinel.another_id
+
+
+def test_get_model_quirks():
+    """Test model quirk list registry."""
+
+    quirk_list = zigpy.quirks.get_model_quirks("some model")
+    assert not quirk_list
+
+    class SomeModel(zigpy.quirks.CustomDevice):
+        signature = {
+            SIG_MODEL: "some model",
+            SIG_MANUFACTURER: "some manufacturer",
+            SIG_ENDPOINTS: {
+                1: {
+                    SIG_EP_PROFILE: 0x0260,
+                    SIG_EP_TYPE: 0x0000,
+                    SIG_EP_INPUT: [0, 1, 3, 4],
+                    SIG_EP_OUTPUT: [0x19],
+                }
+            },
+        }
+
+    quirk_list = zigpy.quirks.get_model_quirks("some model")
+    assert quirk_list
+    assert quirk_list[0] is SomeModel

tests/test_appdb.py
@@ -65,7 +65,6 @@ def mock_dev_init(status: Status):
 
     def _initialize(self):
         self.status = status
-        self.initializing = False
         self.node_desc = zdo_t.NodeDescriptor(0, 1, 2, 3, 4, 5, 6, 7, 8)
 
     return _initialize

tests/test_application.py
@@ -15,6 +15,7 @@ from zigpy.config import (
 )
 from zigpy.exceptions import DeliveryError
 import zigpy.ota
+import zigpy.quirks
 import zigpy.types as t
 
 from .async_mock import AsyncMock, MagicMock, patch, sentinel
@@ -467,3 +468,27 @@ def test_app_update_config(app):
     with pytest.raises(vol.Invalid):
         app.update_config({CONF_OTA: {CONF_OTA_IKEA: "invalid bool"}})
         assert app.config[CONF_OTA][CONF_OTA_IKEA] is True
+
+
+def test_uninitialized_message_handlers(app, ieee):
+    """Test uninitialized message handlers."""
+    handler_1 = MagicMock(return_value=None)
+    handler_2 = MagicMock(return_value=True)
+
+    zigpy.quirks.register_uninitialized_device_message_handler(handler_1)
+    zigpy.quirks.register_uninitialized_device_message_handler(handler_2)
+
+    device = app.add_device(ieee, 0x1234)
+
+    app.handle_message(device, 0x0260, 0x0000, 0, 0, b"123abcd23")
+    assert handler_1.call_count == 0
+    assert handler_2.call_count == 0
+
+    app.handle_message(device, 0x0260, 0x0000, 1, 1, b"123abcd23")
+    assert handler_1.call_count == 1
+    assert handler_2.call_count == 1
+
+    handler_1.return_value = True
+    app.handle_message(device, 0x0260, 0x0000, 1, 1, b"123abcd23")
+    assert handler_1.call_count == 2
+    assert handler_2.call_count == 1

zigpy/application.py
@@ -158,7 +158,18 @@ class ControllerApplication(zigpy.util.ListenableMixin, abc.ABC):
     def deserialize(self, sender, endpoint_id, cluster_id, data):
         return sender.deserialize(endpoint_id, cluster_id, data)
 
-    def handle_message(self, sender, profile, cluster, src_ep, dst_ep, message):
+    def handle_message(
+        self,
+        sender: zigpy.device.Device,
+        profile: int,
+        cluster: int,
+        src_ep: int,
+        dst_ep: int,
+        message: bytes,
+    ) -> None:
+        self.listener_event(
+            "handle_message", sender, profile, cluster, src_ep, dst_ep, message
+        )
         if sender.status == zigpy.device.Status.NEW and dst_ep != 0:
             # only allow ZDO responses while initializing device
             LOGGER.debug(
@@ -166,6 +177,9 @@ class ControllerApplication(zigpy.util.ListenableMixin, abc.ABC):
                 sender.ieee,
                 sender.status,
                 dst_ep,
+            )
+            zigpy.quirks.handle_message_from_uninitialized_sender(
+                sender, profile, cluster, src_ep, dst_ep, message
             )
             return
         elif (

zigpy/device.py
@@ -47,7 +47,6 @@ class Device(zigpy.util.LocalLogMixin, zigpy.util.ListenableMixin):
         self.rssi = None
         self.last_seen = None
         self.status = Status.NEW
-        self.initializing = False
         self._group_scan_handle: Optional[asyncio.Task] = None
         self._listeners = {}
         self._manufacturer = None
@@ -59,13 +58,20 @@ class Device(zigpy.util.LocalLogMixin, zigpy.util.ListenableMixin):
         self._relays = None
         self._skip_configuration = False
 
+    @property
+    def initializing(self) -> bool:
+        """Return True if device is being initialized."""
+        return bool(self._init_handle and not self._init_handle.done())
+
     def schedule_initialize(self):
+        self.cancel_initialization()
+        self._init_handle = asyncio.create_task(self._initialize())
+
+    def cancel_initialization(self) -> None:
+        """Cancel initialization call."""
         if self.initializing:
             LOGGER.debug("Canceling old initialize call")
             self._init_handle.cancel()
-        else:
-            self.initializing = True
-        self._init_handle = asyncio.ensure_future(self._initialize())
 
     def schedule_group_membership_scan(self) -> None:
         """Rescan device group's membership."""
@@ -108,7 +114,6 @@ class Device(zigpy.util.LocalLogMixin, zigpy.util.ListenableMixin):
                         "Endpoint request failed: %s", status
                     )
             except (asyncio.TimeoutError, zigpy.exceptions.ZigbeeException):
-                self.initializing = False
                 self.warning("Failed to discover active endpoints", exc_info=True)
                 return
 
@@ -136,13 +141,11 @@ class Device(zigpy.util.LocalLogMixin, zigpy.util.ListenableMixin):
             if epid
         ]
         if any(ep_failed_init):
-            self.initializing = False
             self.application.listener_event("device_init_failure", self)
             await self.application.remove(self.ieee)
             return
 
         self.status = Status.ENDPOINTS_INIT
-        self.initializing = False
         self._application.device_initialized(self)
 
     def add_endpoint(self, endpoint_id):

commit 0b11e870d327b268abed4c412cb603fe2f45ff56

Fixe regression introduced in #555 (#567)
* Add tests for the issue

* Refactor get_model_quirks() method

* Coverage

zigpy/quirks/__init__.py
@@ -36,16 +36,22 @@ _DEVICE_REGISTRY = DeviceRegistry()
 _uninitialized_device_message_handlers = []
 
 
-def get_device(device, registry=_DEVICE_REGISTRY):
+def get_device(device: zigpy.device.Device, registry: Optional[DeviceRegistry] = None):
     """Get a CustomDevice object, if one is available"""
+    if registry is None:
+        return _DEVICE_REGISTRY.get_device(device)
+
     return registry.get_device(device)
 
 
-def get_model_quirks(
-    model: str, registry: DeviceRegistry = _DEVICE_REGISTRY
-) -> List["CustomDevice"]:
-    """Get all quirks for given model."""
-    return registry.model_quirks[model]
+def get_quirk_list(
+    manufacturer: str, model: str, registry: Optional[DeviceRegistry] = None
+):
+    """Get the Quirk list for a given manufacturer and model."""
+    if registry is None:
+        return _DEVICE_REGISTRY.registry[manufacturer][model]
+
+    return registry.registry[manufacturer][model]
 
 
 def register_uninitialized_device_message_handler(handler: Callable) -> None:

zigpy/quirks/registry.py
@@ -25,9 +25,8 @@ TYPE_MANUF_QUIRKS_DICT = Dict[str, TYPE_MODEL_QUIRKS_LIST]
 
 class DeviceRegistry:
     def __init__(self, *args, **kwargs):
-        self._model_quirks: TYPE_MODEL_QUIRKS_LIST = collections.defaultdict(list)
         self._registry: TYPE_MANUF_QUIRKS_DICT = collections.defaultdict(
-            lambda: self._model_quirks
+            lambda: collections.defaultdict(list)
         )
 
     def add_to_registry(self, custom_device: CustomDeviceType) -> None:
@@ -156,11 +155,6 @@ class DeviceRegistry:
     @property
     def registry(self):
         return self._registry
-
-    @property
-    def model_quirks(self) -> TYPE_MODEL_QUIRKS_LIST:
-        """Return a list of quirks for a given model."""
-        return self._model_quirks
 
     def __contains__(self, device: CustomDeviceType) -> bool:
         manufacturer, model = device.signature.get(

tests/test_quirks.py
@@ -13,6 +13,7 @@ from zigpy.quirks.registry import (
     SIG_EP_TYPE,
     SIG_MANUFACTURER,
     SIG_MODEL,
+    SIG_MODELS_INFO,
     SIG_SKIP_CONFIG,
     DeviceRegistry,
 )
@@ -53,6 +54,23 @@ def real_device():
     real_device[1].device_type = 255
     real_device.model = "model"
     real_device.manufacturer = "manufacturer"
+    real_device[1].add_input_cluster(3)
+    real_device[1].add_output_cluster(6)
+    return real_device
+
+
+@pytest.fixture
+def real_device_2():
+    application = sentinel.application
+    ieee = sentinel.ieee_2
+    nwk = 0x3344
+    real_device = zigpy.device.Device(application, ieee, nwk)
+
+    real_device.add_endpoint(1)
+    real_device[1].profile_id = 255
+    real_device[1].device_type = 255
+    real_device.model = "model"
+    real_device.manufacturer = "A different manufacturer"
     real_device[1].add_input_cluster(3)
     real_device[1].add_output_cluster(6)
     return real_device
@@ -114,6 +132,7 @@ def test_get_device_new_sig(real_device):
     TestDevice.signature[SIG_ENDPOINTS][2] = {SIG_EP_PROFILE: 2}
     registry = _dev_reg(TestDevice)
     assert registry.get_device(real_device) is real_device
+    assert zigpy.quirks.get_device(real_device, registry) is real_device
 
 
 def test_model_manuf_device_sig(real_device):
@@ -659,26 +678,65 @@ async def test_configure_reporting_manufacture_specific(
         assert cmd_mock.call_args[1]["manufacturer"] is sentinel.another_id
 
 
-def test_get_model_quirks():
-    """Test model quirk list registry."""
+def test_different_manuf_same_model(real_device, real_device_2):
+    """Test quirk matching for same model, but different manufacturers."""
 
-    quirk_list = zigpy.quirks.get_model_quirks("some model")
-    assert not quirk_list
-
-    class SomeModel(zigpy.quirks.CustomDevice):
+    class TestDevice_1(zigpy.quirks.CustomDevice):
         signature = {
-            SIG_MODEL: "some model",
-            SIG_MANUFACTURER: "some manufacturer",
+            SIG_MODELS_INFO: (("manufacturer", "model"),),
             SIG_ENDPOINTS: {
                 1: {
-                    SIG_EP_PROFILE: 0x0260,
-                    SIG_EP_TYPE: 0x0000,
-                    SIG_EP_INPUT: [0, 1, 3, 4],
-                    SIG_EP_OUTPUT: [0x19],
+                    SIG_EP_PROFILE: 255,
+                    SIG_EP_TYPE: 255,
+                    SIG_EP_INPUT: [3],
+                    SIG_EP_OUTPUT: [6],
                 }
             },
         }
 
-    quirk_list = zigpy.quirks.get_model_quirks("some model")
-    assert quirk_list
-    assert quirk_list[0] is SomeModel
+        def get_signature(self):
+            pass
+
+    class TestDevice_2(zigpy.quirks.CustomDevice):
+        signature = {
+            SIG_MODELS_INFO: (("A different manufacturer", "model"),),
+            SIG_ENDPOINTS: {
+                1: {
+                    SIG_EP_PROFILE: 255,
+                    SIG_EP_TYPE: 255,
+                    SIG_EP_INPUT: [3],
+                    SIG_EP_OUTPUT: [6],
+                }
+            },
+        }
+
+        def get_signature(self):
+            pass
+
+    registry = DeviceRegistry()
+    registry.add_to_registry(TestDevice_1)
+
+    assert isinstance(registry.get_device(real_device), TestDevice_1)
+
+    assert registry.get_device(real_device_2) is real_device_2
+    registry.add_to_registry(TestDevice_2)
+    assert isinstance(registry.get_device(real_device_2), TestDevice_2)
+
+    assert not zigpy.quirks.get_quirk_list("manufacturer", "no such model")
+    assert not zigpy.quirks.get_quirk_list("manufacturer", "no such model", registry)
+    assert not zigpy.quirks.get_quirk_list("A different manufacturer", "no such model")
+    assert not zigpy.quirks.get_quirk_list(
+        "A different manufacturer", "no such model", registry
+    )
+    assert not zigpy.quirks.get_quirk_list("no such manufacturer", "model")
+    assert not zigpy.quirks.get_quirk_list("no such manufacturer", "model", registry)
+
+    manuf1_list = zigpy.quirks.get_quirk_list("manufacturer", "model", registry)
+    assert len(manuf1_list) == 1
+    assert manuf1_list[0] is TestDevice_1
+
+    manuf2_list = zigpy.quirks.get_quirk_list(
+        "A different manufacturer", "model", registry
+    )
+    assert len(manuf2_list) == 1
+    assert manuf2_list[0] is TestDevice_2