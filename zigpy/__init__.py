b92d142c9b0e18345f78bcc06c651f4643a84014
sarakha63 <sarakha_ludovic@yahoo.fr>
2020-12-16 14:38:06 +0100

    rebase continue

--- zigpy/__init__.py
+++ zigpy/__init__.py
@@ -1,10 +1,6 @@
 # coding: utf-8
 MAJOR_VERSION = 0
-<<<<<<< Upstream, based on 5becce4c89c8c3d8cce474e7de8f7e9913631e8e
 MINOR_VERSION = 29
-=======
-MINOR_VERSION = 28
->>>>>>> 0dc9d10 0.28.0.dev0 version bump
 PATCH_VERSION = "0.dev0"
 __short_version__ = f"{MAJOR_VERSION}.{MINOR_VERSION}"
 __version__ = f"{__short_version__}.{PATCH_VERSION}"
