d8a1c022340f874cb581b2eb01e967258d75bf36
David Mulcahey <david.mulcahey@me.com>
2021-11-19 15:29:36 -0500

    remove whitespace

--- zigpy/zcl/__init__.py
+++ zigpy/zcl/__init__.py
@@ -538,7 +538,7 @@ class Cluster(util.ListenableMixin, util.CatchingTaskMixin, metaclass=Registry):
     @property
     def commands(self):
         return list(self._server_commands_idx.keys())
-    
+
     def update_attribute(self, attrid, value):
         """Update specified attribute with specified value"""
         self._update_attribute(attrid, value)
