f5e518216e9a94f2b3544093232a6692ebeaebc9
puddly <32534428+puddly@users.noreply.github.com>
2022-01-02 15:27:38 -0500

    Re-run the appdb test suite with the pysqlite3 module if it exists

---
+++ tests/test_appdb_pysqlite.py
@@ -0,0 +1,40 @@
+import sqlite3
+
+import pytest
+
+from tests.async_mock import patch
+
+try:
+    import pysqlite3
+except ImportError:
+    pass
+else:
+
+    @pytest.fixture(scope="module", autouse=True)
+    def force_use_pysqlite3():
+        with (
+            # Make the sqlite3 module "be" pysqlite3
+            patch.multiple(
+                target=sqlite3,
+                **{
+                    attr: getattr(pysqlite3, attr)
+                    for attr in dir(pysqlite3)
+                    if hasattr(sqlite3, attr)
+                }
+            ),
+            # Directly replace it as well in `zigpy.appdb`
+            patch("zigpy.appdb.sqlite3", pysqlite3),
+        ):
+            # Ensure the module was patched
+            assert sqlite3.connect is pysqlite3.connect
+            yield
+
+        # Ensure the module is unpatched
+        assert sqlite3.connect is not pysqlite3.connect
+
+    # Re-run most of the appdb tests
+    from tests.test_appdb import *  # noqa: F401,F403
+    from tests.test_appdb_migration import *  # noqa: F401,F403
+
+    del test_pysqlite_load_success  # noqa: F821
+    del test_pysqlite_load_failure  # noqa: F821
