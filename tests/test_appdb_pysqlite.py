4b23310f4bfa5dc32ab42d06b35e565d0a50caca
puddly <32534428+puddly@users.noreply.github.com>
2022-01-03 23:43:05 -0500

    Use two nested context managers to make Python 3.8 happy

--- tests/test_appdb_pysqlite.py
+++ tests/test_appdb_pysqlite.py
@@ -12,22 +12,21 @@ else:
 
     @pytest.fixture(scope="module", autouse=True)
     def force_use_pysqlite3():
-        with (
-            # Make the sqlite3 module "be" pysqlite3
-            patch.multiple(
-                target=sqlite3,
-                **{
-                    attr: getattr(pysqlite3, attr)
-                    for attr in dir(pysqlite3)
-                    if hasattr(sqlite3, attr)
-                }
-            ),
-            # Directly replace it as well in `zigpy.appdb`
-            patch("zigpy.appdb.sqlite3", pysqlite3),
+        # Make the sqlite3 module "be" pysqlite3
+        with patch.multiple(
+            target=sqlite3,
+            **{
+                attr: getattr(pysqlite3, attr)
+                for attr in dir(pysqlite3)
+                if hasattr(sqlite3, attr)
+            }
         ):
             # Ensure the module was patched
             assert sqlite3.connect is pysqlite3.connect
-            yield
+
+            # Directly replace it as well in `zigpy.appdb`
+            with patch("zigpy.appdb.sqlite3", pysqlite3):
+                yield
 
         # Ensure the module is unpatched
         assert sqlite3.connect is not pysqlite3.connect
