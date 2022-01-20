d1806da008f7406c9c19d02a2cdcf73bd26c37bf
mdeweerd <mdeweerd@users.noreply.github.com>
2022-01-20 19:39:45 +0100

    Add write_attributes_raw method Enables writing attributes that are not predefined. Similar to read_attributes_raw

--- zigpy/zcl/__init__.py
+++ zigpy/zcl/__init__.py
@@ -374,7 +374,14 @@ class Cluster(util.ListenableMixin, util.CatchingTaskMixin, metaclass=Registry):
     async def write_attributes(
         self, attributes: Dict[Union[str, int], Any], manufacturer: Optional[int] = None
     ) -> List:
+        """Write attributes to device with internal 'attributes' validation"""
         args = self._write_attr_records(attributes)
+        return self.write_attributes_raw(args, manufacturer)
+
+    async def write_attributes_raw(
+        self, args: List[foundation.Attribute], manufacturer: Optional[int] = None
+    ) -> List:
+        """Write attributes to device without internal 'attributes' validation"""
         result = await self._write_attributes(args, manufacturer=manufacturer)
         if not isinstance(result[0], list):
             return result
