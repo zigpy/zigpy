28e4430f7d31a16b19f3e0eca6b821ae05c03e07
PrairieSnpr <midnryanmiller@gmail.com>
2022-09-18 15:47:43 -0400

    Add validator

--- zigpy/config/__init__.py
+++ zigpy/config/__init__.py
@@ -24,7 +24,7 @@ from zigpy.config.defaults import (
     CONF_TOPO_SCAN_PERIOD_DEFAULT,
     CONF_TOPO_SKIP_COORDINATOR_DEFAULT,
 )
-from zigpy.config.validators import cv_boolean, cv_hex, cv_key
+from zigpy.config.validators import cv_boolean, cv_hex, cv_key, cv_simple_descriptor
 import zigpy.types as t
 from zigpy.zdo.types import SimpleDescriptor
 
@@ -119,9 +119,7 @@ ZIGPY_SCHEMA = vol.Schema(
         vol.Optional(
             CONF_NWK_VALIDATE_SETTINGS, default=CONF_NWK_VALIDATE_SETTINGS_DEFAULT
         ): cv_boolean,
-        vol.Optional(CONF_ADDITIONAL_ENDPOINTS, default=[]): [
-            lambda v: SimpleDescriptor(**v)
-        ],
+        vol.Optional(CONF_ADDITIONAL_ENDPOINTS, default=[]): [cv_simple_descriptor],
     },
     extra=vol.ALLOW_EXTRA,
 )
