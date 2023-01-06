a2e5910aab25aceca855e6df0847df4487022818
puddly <32534428+puddly@users.noreply.github.com>
2023-01-05 22:55:10 -0500

    0.54.0.dev0 version bump

--- zigpy/__init__.py
+++ zigpy/__init__.py
@@ -1,5 +1,5 @@
 MAJOR_VERSION = 0
-MINOR_VERSION = 53
+MINOR_VERSION = 54
 PATCH_VERSION = "0.dev0"
 __short_version__ = f"{MAJOR_VERSION}.{MINOR_VERSION}"
 __version__ = f"{__short_version__}.{PATCH_VERSION}"
