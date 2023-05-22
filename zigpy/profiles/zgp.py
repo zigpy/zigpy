c13e11b0fc79b64808866f3235b25f09ac00cb0f
nworbneb <ben@xarin.com>
2023-05-22 12:15:31 +0100

    Create zgp.py

---
+++ zigpy/profiles/zgp.py
@@ -0,0 +1,18 @@
+from __future__ import annotations
+
+import zigpy.types as t
+
+PROFILE_ID = 41440
+
+class DeviceType(t.enum16):
+    PROXY = 0x0060
+    PROXY_MIN = 0x0061
+    TARGET_PLUS = 0x0062
+    TARGET = 0x0063
+    COMM_TOOL = 0x0064
+    COMBO = 0x0065
+    COMBO_MIN = 0x0066
+
+CLUSTERS = {
+    DeviceType.PROXY_MIN: ([0x0021], []),
+}
