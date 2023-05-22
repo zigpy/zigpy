b8d6f2eb4133fa161db87e2166fa5693410b1072
nworbneb <ben@xarin.com>
2023-05-22 12:32:27 +0100

    Update __init__.py

--- zigpy/profiles/__init__.py
+++ zigpy/profiles/__init__.py
@@ -1,5 +1,5 @@
 from __future__ import annotations
 
-from . import zha, zll
+from . import zha, zll, zgp
 
-PROFILES = {zha.PROFILE_ID: zha, zll.PROFILE_ID: zll}
+PROFILES = {zha.PROFILE_ID: zha, zll.PROFILE_ID: zll, zgp.PROFILE_ID: zgp}
