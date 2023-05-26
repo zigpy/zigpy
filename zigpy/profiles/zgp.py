a273d44830c9c8cc6e2eda17c28e4e07e8c0dad0
nworbneb <ben@xarin.com>
2023-05-26 17:05:30 +0100

    Updated with latest naming (MIN -> BASIC)

--- zigpy/profiles/zgp.py
+++ zigpy/profiles/zgp.py
@@ -7,12 +7,12 @@ PROFILE_ID = 41440
 
 class DeviceType(t.enum16):
     PROXY = 0x0060
-    PROXY_MIN = 0x0061
+    PROXY_BASIC = 0x0061
     TARGET_PLUS = 0x0062
     TARGET = 0x0063
     COMM_TOOL = 0x0064
     COMBO = 0x0065
-    COMBO_MIN = 0x0066
+    COMBO_BASIC = 0x0066
 
 
 CLUSTERS = {
