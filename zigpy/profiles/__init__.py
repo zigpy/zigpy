b79a072fe19ed4f2adddbb36a7c1a8d901c32fa9
nworbneb <ben@xarin.com>
2023-05-28 20:22:26 +0100

    Add greenpower cluster enumerations (#1213)
    * Add greenpower cluster enumerations
    
    * Create zgp.py
    
    * Remove greenpower changes
    
    * Update __init__.py
    
    * black fixes
    
    * Update zigpy/profiles/zgp.py
    
    * Update zigpy/profiles/zgp.py
    
    * Updated with latest naming (MIN -> BASIC)
    
    * Update cluster with new naming
    
    * Update clusters dictionary
    
    ---------
    
    Co-authored-by: puddly <32534428+puddly@users.noreply.github.com>

--- zigpy/profiles/__init__.py
+++ zigpy/profiles/__init__.py
@@ -1,5 +1,5 @@
 from __future__ import annotations
 
-from . import zha, zll
+from . import zgp, zha, zll
 
-PROFILES = {zha.PROFILE_ID: zha, zll.PROFILE_ID: zll}
+PROFILES = {zha.PROFILE_ID: zha, zll.PROFILE_ID: zll, zgp.PROFILE_ID: zgp}
