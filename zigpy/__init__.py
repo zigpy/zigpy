3dd22ee79fb530e4ab20eba59ab943ab8aa3b276
puddly <32534428+puddly@users.noreply.github.com>
2023-07-06 13:32:02 -0400

    Migrate to `pyproject.toml`

--- zigpy/__init__.py
+++
@@ -1,7 +0,0 @@
-from __future__ import annotations
-
-MAJOR_VERSION = 0
-MINOR_VERSION = 56
-PATCH_VERSION = "0.dev0"
-__short_version__ = f"{MAJOR_VERSION}.{MINOR_VERSION}"
-__version__ = f"{__short_version__}.{PATCH_VERSION}"
