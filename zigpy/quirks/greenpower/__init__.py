5578867012d5259e6c7c456411cb2e603f57b6ec
Conrad Kreyling <conrad@kreyling.biz>
2023-11-28 20:24:42 -0500

    Stub out PTM215ZE device in prep for arrival

--- zigpy/quirks/greenpower/__init__.py
+++ zigpy/quirks/greenpower/__init__.py
@@ -1,8 +1,9 @@
 from __future__ import annotations
 from .ptm215z import EnoceanPTM215ZDevice
+from .ptm125ze import EnoceanPTM215ZEDevice
 from .tuya_selfpowered import TuyaSelfPoweredSwitch
 import zigpy.quirks
 
-ALL_GP_DEVICE_TYPES = (EnoceanPTM215ZDevice,TuyaSelfPoweredSwitch)
+ALL_GP_DEVICE_TYPES = (EnoceanPTM215ZDevice,EnoceanPTM215ZEDevice,TuyaSelfPoweredSwitch)
 
 zigpy.quirks._GP_REGISTRY =ALL_GP_DEVICE_TYPES
---
+++ zigpy/quirks/greenpower/ptm125ze.py
@@ -0,0 +1,152 @@
+from __future__ import annotations
+import typing
+from zigpy.profiles.zgp import (
+    GREENPOWER_ENDPOINT_ID,
+    GREENPOWER_CLUSTER_ID
+)
+from zigpy.zcl.clusters.greenpower import GPNotificationSchema
+from zigpy.zgp import GPDeviceType
+from zigpy.zgp.device import GreenPowerDevice
+from zigpy.zgp.types import GPSecurityKeyType, GPSecurityLevel, GreenPowerExtData
+
+if typing.TYPE_CHECKING:
+    from zigpy.application import ControllerApplication
+
+class EnoceanPTM215ZEDevice(GreenPowerDevice):
+    @classmethod
+    def match(cls, device: GreenPowerDevice) -> bool:
+        # First check simple security and type parameters
+        if device.ext_data.device_id != GPDeviceType.SWITCH_ON_OFF:
+            return False
+        # XXX: NEED A SNIFF TO KNOW FOR SURE
+        # if device.ext_data.security_key_type != GPSecurityKeyType.IndividualKey:
+        #     return False
+        # if device.ext_data.security_level != GPSecurityLevel.FullFrameCounterAndMIC:
+        #     return False
+        # Finally match against GPD ID prefix
+        return str(device.ext_data.gpd_id).startswith("0x015")
+    
+    def __init__(self, application: ControllerApplication, ext: GreenPowerExtData):
+        super().__init__(application, ext)
+        self.manufacturer = "EnOcean"
+        self.model = "PTM215ZE"
+    
+    device_automation_triggers = {
+        ("initial_switch_press", "button_1"): {
+            "command": "notification",
+            "cluster_id": GREENPOWER_CLUSTER_ID,
+            "params": {"command_id": 0x22}
+        },
+        ("release", "button_1"): {
+            "command": "notification",
+            "cluster_id": GREENPOWER_CLUSTER_ID,
+            "params": {"command_id": 0x23}
+        },
+        ("initial_switch_press", "button_2"): {
+            "command": "notification",
+            "cluster_id": GREENPOWER_CLUSTER_ID,
+            "params": {"command_id": 0x18}
+        },
+        ("release", "button_2"): {
+            "command": "notification",
+            "cluster_id": GREENPOWER_CLUSTER_ID,
+            "params": {"command_id": 0x19}
+        },
+        ("initial_switch_press", "button_3"): {
+            "command": "notification",
+            "cluster_id": GREENPOWER_CLUSTER_ID,
+            "params": {"command_id": 0x14}
+        },
+        ("release", "button_3"): {
+            "command": "notification",
+            "cluster_id": GREENPOWER_CLUSTER_ID,
+            "params": {"command_id": 0x15}
+        },
+        ("initial_switch_press", "button_4"): {
+            "command": "notification",
+            "cluster_id": GREENPOWER_CLUSTER_ID,
+            "params": {"command_id": 0x12}
+        },
+        ("release", "button_4"): {
+            "command": "notification",
+            "cluster_id": GREENPOWER_CLUSTER_ID,
+            "params": {"command_id": 0x13}
+        },
+
+        ("initial_switch_press", "button_1_and_2"): {
+            "command": "notification",
+            "cluster_id": GREENPOWER_CLUSTER_ID,
+            "params": {"command_id": 0x64}
+        },
+        ("release", "button_1_and_2"): {
+            "command": "notification",
+            "cluster_id": GREENPOWER_CLUSTER_ID,
+            "params": {"command_id": 0x65}
+        },
+
+        ("initial_switch_press", "button_1_and_3"): {
+            "command": "notification",
+            "cluster_id": GREENPOWER_CLUSTER_ID,
+            "params": {"command_id": 0x62}
+        },
+        ("release", "button_1_and_3"): {
+            "command": "notification",
+            "cluster_id": GREENPOWER_CLUSTER_ID,
+            "params": {"command_id": 0x63}
+        },
+
+        ("initial_switch_press", "button_1_and_4"): {
+            "command": "notification",
+            "cluster_id": GREENPOWER_CLUSTER_ID,
+            "params": {"command_id": 0x1e}
+        },
+        ("release", "button_1_and_4"): {
+            "command": "notification",
+            "cluster_id": GREENPOWER_CLUSTER_ID,
+            "params": {"command_id": 0x1f}
+        },
+
+        ("initial_switch_press", "button_2_and_3"): {
+            "command": "notification",
+            "cluster_id": GREENPOWER_CLUSTER_ID,
+            "params": {"command_id": 0x1c}
+        },
+        ("release", "button_2_and_3"): {
+            "command": "notification",
+            "cluster_id": GREENPOWER_CLUSTER_ID,
+            "params": {"command_id": 0x1d}
+        },
+
+        ("initial_switch_press", "button_2_and_4"): {
+            "command": "notification",
+            "cluster_id": GREENPOWER_CLUSTER_ID,
+            "params": {"command_id": 0x1a}
+        },
+        ("release", "button_2_and_4"): {
+            "command": "notification",
+            "cluster_id": GREENPOWER_CLUSTER_ID,
+            "params": {"command_id": 0x1b}
+        },
+
+        ("initial_switch_press", "button_3_and_4"): {
+            "command": "notification",
+            "cluster_id": GREENPOWER_CLUSTER_ID,
+            "params": {"command_id": 0x16}
+        },
+        ("release", "button_3_and_4"): {
+            "command": "notification",
+            "cluster_id": GREENPOWER_CLUSTER_ID,
+            "params": {"command_id": 0x17}
+        },
+
+        ("initial_switch_press", "energy_bar"): {
+            "command": "notification",
+            "cluster_id": GREENPOWER_CLUSTER_ID,
+            "params": {"command_id": 0x10}
+        },
+        ("release", "energy_bar"): {
+            "command": "notification",
+            "cluster_id": GREENPOWER_CLUSTER_ID,
+            "params": {"command_id": 0x11}
+        },
+    }
