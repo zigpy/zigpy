58d9466eaf59945e787e63d6282afb3368b5cddd
puddly <32534428+puddly@users.noreply.github.com>
2023-12-18 10:13:55 -0500

    Always poll the watchdog

--- zigpy/application.py
+++ zigpy/application.py
@@ -652,10 +652,6 @@ class ControllerApplication(zigpy.util.ListenableMixin, abc.ABC):
         while True:
             await asyncio.sleep(self._watchdog_period)
 
-            if self._concurrent_requests_semaphore.locked():
-                LOGGER.debug("Skipping watchdog poll, request semaphore is blocked")
-                continue
-
             try:
                 LOGGER.debug("Feeding watchdog")
                 await self._watchdog_feed()
