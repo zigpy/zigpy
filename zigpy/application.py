56a73208888d23ef0fc8a689908a4d96f98ef903
puddly <32534428+puddly@users.noreply.github.com>
2024-07-14 11:13:47 -0400

    Create a base class for serial protocols

--- zigpy/application.py
+++ zigpy/application.py
@@ -450,6 +450,7 @@ class ControllerApplication(zigpy.util.ListenableMixin, abc.ABC):
             except Exception:  # noqa: BLE001
                 LOGGER.warning("Failed to disconnect from database", exc_info=True)
 
+        LOGGER.debug("Waiting for radio disconnection event")
         await self._connection_lost_event.wait()
         self._connection_lost_event.clear()
 
