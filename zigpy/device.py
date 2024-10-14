3a7649f2e77973f75f7e123e269913f2be968d7a
David F. Mulcahey <david.mulcahey@icloud.com>
2024-10-14 10:54:57 -0400

    Limit device network concurrency (#1480)
    * Limit device concurrency
    
    * differentiate logs
    
    * Expose `priority` for devices as well
    
    * Move device concurrency out into its own constant
    
    * Add a unit test
    
    ---------
    
    Co-authored-by: puddly <32534428+puddly@users.noreply.github.com>

--- zigpy/device.py
+++ zigpy/device.py
@@ -1,11 +1,13 @@
 from __future__ import annotations
 
 import asyncio
+import contextlib
 from datetime import datetime, timezone
 import enum
 import itertools
 import logging
 import sys
+import time
 import typing
 import warnings
 
@@ -48,6 +50,7 @@ if typing.TYPE_CHECKING:
 LOGGER = logging.getLogger(__name__)
 
 PACKET_DEBOUNCE_WINDOW = 10
+MAX_DEVICE_CONCURRENCY = 1
 
 AFTER_OTA_ATTR_READ_DELAY = 10
 OTA_RETRY_DECORATOR = zigpy.util.retryable_request(
@@ -93,9 +96,35 @@ class Device(zigpy.util.LocalLogMixin, zigpy.util.ListenableMixin):
         self._send_sequence: int = 0
 
         self._packet_debouncer = zigpy.datastructures.Debouncer()
+        self._concurrent_requests_semaphore = (
+            zigpy.datastructures.PriorityDynamicBoundedSemaphore(MAX_DEVICE_CONCURRENCY)
+        )
 
         # Retained for backwards compatibility, will be removed in a future release
         self.status = Status.NEW
+
+    @contextlib.asynccontextmanager
+    async def _limit_concurrency(self, *, priority: int = 0):
+        """Async context manager to limit device request concurrency."""
+
+        start_time = time.monotonic()
+        was_locked = self._concurrent_requests_semaphore.locked()
+
+        if was_locked:
+            LOGGER.debug(
+                "Device concurrency (%s) reached, delaying device request (%s enqueued)",
+                self._concurrent_requests_semaphore.max_value,
+                self._concurrent_requests_semaphore.num_waiting,
+            )
+
+        async with self._concurrent_requests_semaphore(priority=priority):
+            if was_locked:
+                LOGGER.debug(
+                    "Previously delayed device request is now running, delayed by %0.2fs",
+                    time.monotonic() - start_time,
+                )
+
+            yield
 
     def get_sequence(self) -> t.uint8_t:
         self._send_sequence = (self._send_sequence + 1) % 256
@@ -339,16 +368,17 @@ class Device(zigpy.util.LocalLogMixin, zigpy.util.ListenableMixin):
             priority=priority,
         )
 
-        if not expect_reply:
-            await send_request()
-            return None
+        async with self._limit_concurrency(priority=priority):
+            if not expect_reply:
+                await send_request()
+                return None
 
-        # Only create a pending request if we are expecting a reply
-        with self._pending.new(sequence) as req:
-            await send_request()
+            # Only create a pending request if we are expecting a reply
+            with self._pending.new(sequence) as req:
+                await send_request()
 
-            async with asyncio_timeout(timeout):
-                return await req.result
+                async with asyncio_timeout(timeout):
+                    return await req.result
 
     def handle_message(
         self,
