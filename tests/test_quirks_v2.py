1d479afbf2b7b826a475daea84d2703db5c8bc83
puddly <32534428+puddly@users.noreply.github.com>
2025-02-05 13:27:39 -0500

    Make the `quirk_file_line` test dynamic

--- tests/test_quirks_v2.py
+++ tests/test_quirks_v2.py
@@ -1,5 +1,6 @@
 """Tests for the quirks v2 module."""
 
+import pathlib
 from typing import Final
 from unittest.mock import AsyncMock
 
@@ -103,6 +104,7 @@ async def test_quirks_v2(device_mock):
             report: Final = ZCLAttributeDef(id=0x0000, type=t.uint8_t)
 
     entry = (
+        # Quirk builder creation line, this comment is read by this unit test
         QuirkBuilder(device_mock.manufacturer, device_mock.model, registry=registry)
         .filter(signature_matches(signature))
         .adds(
@@ -132,7 +134,15 @@ async def test_quirks_v2(device_mock):
     assert str(quirked.quirk_metadata.quirk_file).endswith(
         "zigpy/tests/test_quirks_v2.py"
     )
-    assert quirked.quirk_metadata.quirk_file_line == 106
+
+    # To avoid having to rewrite this test every time quirks change, we read the current
+    # file to find the line number
+    quirk_builder_line = next(
+        index
+        for index, line in enumerate(pathlib.Path(__file__).read_text().splitlines())
+        if "# Quirk builder creation line" in line
+    )
+    assert quirked.quirk_metadata.quirk_file_line == quirk_builder_line + 2
 
     ep = quirked.endpoints[1]
 
