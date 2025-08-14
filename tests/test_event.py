4133362f15a08a0561ec4b2bb8de41aca2859e08
puddly <32534428+puddly@users.noreply.github.com>
2025-08-14 17:25:41 -0400

    Revert "Sleeps for 3.9"
    This reverts commit 1506e98fb54d7f6634c0d6a54a862391a7ec8b59.

--- tests/test_event.py
+++ tests/test_event.py
@@ -129,7 +129,6 @@ async def test_event_base_emit_coro():
     event.once("test", callback)
     event.emit("test", "data")
 
-    await asyncio.sleep(0)
     await asyncio.gather(*event._event_tasks)
 
     assert callback.await_count == 1
@@ -141,7 +140,6 @@ async def test_event_base_emit_coro():
     unsub = event.on_event("test", callback)
     event.emit("test", "data")
 
-    await asyncio.sleep(0)
     await asyncio.gather(*event._event_tasks)
 
     assert callback.await_count == 1
@@ -154,7 +152,6 @@ async def test_event_base_emit_coro():
     unsub = event.on_all_events(callback)
     event.emit("test", "data")
 
-    await asyncio.sleep(0)
     await asyncio.gather(*event._event_tasks)
 
     assert callback.await_count == 1
@@ -168,7 +165,6 @@ async def test_event_base_emit_coro():
 
     event.emit(test_event.event, test_event)
 
-    await asyncio.sleep(0)
     await asyncio.gather(*event._event_tasks)
 
     assert event.handle_test.await_count == 1
@@ -187,7 +183,6 @@ async def test_event_emit_with_context():
     event.once("test", async_callback, with_context=True)
     event.emit("test", "data")
 
-    await asyncio.sleep(0)
     await asyncio.gather(*event._event_tasks)
 
     sync_callback.assert_called_once_with("test", "data")
