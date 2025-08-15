e1b6ed681563683b8ac8b4942742aa65902f3b6a
puddly <32534428+puddly@users.noreply.github.com>
2025-08-15 16:52:54 -0400

    Do not pass unparsed commands to listeners

--- zigpy/device.py
+++ zigpy/device.py
@@ -832,15 +832,16 @@ class Device(zigpy.util.LocalLogMixin, zigpy.util.ListenableMixin):
             return
 
         # Pass the request off to a listener, if one is registered
-        for listener in itertools.chain(
-            self._application._req_listeners[zigpy.listeners.ANY_DEVICE],
-            self._application._req_listeners[self],
-        ):
-            # Resolve only until the first future listener
-            if listener.resolve(hdr, cmd) and isinstance(
-                listener, zigpy.listeners.FutureListener
+        if not isinstance(cmd, bytes):
+            for listener in itertools.chain(
+                self._application._req_listeners[zigpy.listeners.ANY_DEVICE],
+                self._application._req_listeners[self],
             ):
-                break
+                # Resolve only until the first future listener
+                if listener.resolve(hdr, cmd) and isinstance(
+                    listener, zigpy.listeners.FutureListener
+                ):
+                    break
 
         # Finally, pass it off to the cluster message handler. This will be removed.
         if zcl_cluster is not None:
