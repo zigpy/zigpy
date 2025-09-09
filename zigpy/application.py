5d5ce82f29b08b0db330e865ad20ec8791a8eefc
puddly <32534428+puddly@users.noreply.github.com>
2025-09-09 18:11:21 -0400

    Add `can_write_network_settings` to ControllerApplication API (#1616)
    * Implement can_write_network_settings
    
    * Allow passing `None`
    
    * Revert "Allow passing `None`"
    
    This reverts commit a989c7d94f4d8abc97a9b882b8f2e0fa7aaa4293.
    
    * Add `DestructiveWriteNetworkSettings`
    
    * Enforce kwargs
    
    * Add `can_read_network_settings`
    
    * Oops
    
    * Revert "Add `can_read_network_settings`"
    
    This reverts commit 666915b114f3c1c0e5b0afa264aac61d17439ce6.

--- zigpy/application.py
+++ zigpy/application.py
@@ -1344,6 +1344,26 @@ class ControllerApplication(zigpy.util.ListenableMixin, abc.ABC):
         """Permit a node to join with the provided link key."""
         raise NotImplementedError  # pragma: no cover
 
+    async def can_write_network_settings(
+        self,
+        *,
+        network_info: zigpy.state.NetworkInfo,
+        node_info: zigpy.state.NodeInfo,
+    ) -> bool:
+        """Returns `True` if the radio can write the given network settings.
+
+        If restoration is not possible, `CannotWriteNetworkSettings` is raised.
+        If restoration is possible in a destructive way (e.g. write-once tokens),
+        `DestructiveWriteNetworkSettings` is raised.
+
+        Some radio firmwares do not support writing every network setting in a backup
+        (ZiGate cannot set the PAN ID, older EZSP can only write the EUI64 once, etc.).
+        Not all situations, however, are critical failures: if we are restoring a
+        backup where the PAN ID does not change or the EUI64 remains the same, we can
+        restore the backup successfully.
+        """
+        return True
+
     @abc.abstractmethod
     async def write_network_info(
         self,
