753a06ce415be7a6f39d68352f0fb7a557fc89eb
puddly <32534428+puddly@users.noreply.github.com>
2026-02-11 12:27:03 -0500

    Remove pysqlite tests in appdb

--- tests/test_appdb.py
+++ tests/test_appdb.py
@@ -2,8 +2,6 @@ import asyncio
 import contextlib
 from datetime import UTC, datetime, timedelta
 import pathlib
-import sqlite3
-import sys
 import threading
 import time
 
@@ -1055,60 +1053,6 @@ async def test_last_seen(tmp_path):
     dev = app.get_device(ieee=ieee)
     assert dev.last_seen >= next_last_seen + 35  # updated
     await app.shutdown()
-
-
-@pytest.mark.parametrize(
-    ("stdlib_version", "use_sqlite"),
-    [
-        ((1, 0, 0), False),
-        ((2, 0, 0), False),
-        ((3, 0, 0), False),
-        ((3, 24, 0), True),
-        ((4, 0, 0), True),
-    ],
-)
-def test_pysqlite_load_success(stdlib_version, use_sqlite):
-    """Test that the internal import SQLite helper picks the correct module."""
-    pysqlite3 = MagicMock()
-    pysqlite3.sqlite_version_info = (3, 30, 0)
-
-    with (
-        patch.dict(sys.modules, {"pysqlite3": pysqlite3}),
-        patch.object(sys.modules["sqlite3"], "sqlite_version_info", new=stdlib_version),
-    ):
-        module = zigpy.appdb._import_compatible_sqlite3(zigpy.appdb.MIN_SQLITE_VERSION)
-
-    if use_sqlite:
-        assert module is sqlite3
-    else:
-        assert module is pysqlite3
-
-
-@pytest.mark.parametrize(
-    ("stdlib_version", "pysqlite3_version"),
-    [
-        ((1, 0, 0), None),
-        ((1, 0, 0), (1, 0, 1)),
-    ],
-)
-def test_pysqlite_load_failure(stdlib_version, pysqlite3_version):
-    """Test that the internal import SQLite helper will throw an error when no compatible
-    module can be found.
-    """
-
-    if pysqlite3_version is not None:
-        pysqlite3 = MagicMock()
-        pysqlite3.sqlite_version_info = pysqlite3_version
-        pysqlite3_patch = patch.dict(sys.modules, {"pysqlite3": pysqlite3})
-    else:
-        pysqlite3_patch = patch.dict(sys.modules, {"pysqlite3": None})
-
-    with (
-        pysqlite3_patch,
-        patch.object(sys.modules["sqlite3"], "sqlite_version_info", new=stdlib_version),
-    ):
-        with pytest.raises(RuntimeError):
-            zigpy.appdb._import_compatible_sqlite3(zigpy.appdb.MIN_SQLITE_VERSION)
 
 
 async def test_appdb_network_backups(tmp_path, backup_factory):  # noqa: F811
