f3df46c40379cf85b5596c764a690bfb60bdcce7
puddly <32534428+puddly@users.noreply.github.com>
2026-07-27 20:16:43 -0400

    Add coordinator group subscription APIs (#1864)

--- tests/conftest.py
+++ tests/conftest.py
@@ -171,6 +171,16 @@ class App(BaseApp):
         else:
             return 10
 
+    async def _subscribe_to_multicast_group(
+        self, group_id: t.Group, endpoint_id: int
+    ) -> None:
+        pass
+
+    async def _unsubscribe_from_multicast_group(
+        self, group_id: t.Group, endpoint_id: int
+    ) -> None:
+        pass
+
 
 def recursive_dict_merge(
     obj: dict[str, typing.Any], updates: dict[str, typing.Any]
--- zigpy/application.py
+++ zigpy/application.py
@@ -1668,6 +1668,34 @@ class ControllerApplication(zigpy.util.ListenableMixin, abc.ABC):
     async def _packet_capture_change_channel(self, channel: int) -> None:
         """Change the channel of an active packet capture, internal."""
 
+    async def subscribe_to_multicast_group(
+        self, group_id: t.Group, endpoint_id: int = 1
+    ) -> None:
+        """Ask the coordinator firmware to subscribe to a group, if needed."""
+        await self._subscribe_to_multicast_group(
+            group_id=group_id, endpoint_id=endpoint_id
+        )
+
+    # @abc.abstractmethod
+    async def _subscribe_to_multicast_group(
+        self, group_id: t.Group, endpoint_id: int
+    ) -> None:
+        """Ask the coordinator firmware to subscribe to a group, if needed."""
+
+    async def unsubscribe_from_multicast_group(
+        self, group_id: t.Group, endpoint_id: int = 1
+    ) -> None:
+        """Ask the coordinator firmware to unsubscribe from a group, if needed."""
+        await self._unsubscribe_from_multicast_group(
+            group_id=group_id, endpoint_id=endpoint_id
+        )
+
+    # @abc.abstractmethod
+    async def _unsubscribe_from_multicast_group(
+        self, group_id: t.Group, endpoint_id: int
+    ) -> None:
+        """Ask the coordinator firmware to unsubscribe from a group, if needed."""
+
     async def permit(self, time_s: int = 60, node: t.EUI64 | str | None = None) -> None:
         """Permit joining on a specific node or all router nodes."""
         assert 0 <= time_s <= 254
